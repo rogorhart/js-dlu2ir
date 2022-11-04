7acde748e5a5272a7ab2fcaa0a9825d37239d054
rogorhart <113944522+rogorhart@users.noreply.github.com>
2022-11-04 18:51:05 +0000

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-// Import stylesheets
-import './style.css';
+const canvasSketch = require('canvas-sketch');
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// Sketch parameters
+const settings = {
+  dimensions: 'a4',
+  pixelsPerInch: 300,
+  units: 'in'
+};
+
+// Artwork function
+const sketch = () => {
+  return ({ context, width, height }) => {
+    // Margin in inches
+    const margin = 1 / 4;
+
+    // Off-white background
+    context.fillStyle = 'red'; //'hsl(0, 0%, 98%)';
+    context.fillRect(0, 0, width, height);
+
+    // Gradient foreground
+    const fill = context.createLinearGradient(0, 0, width, height);
+    fill.addColorStop(0, 'cyan');
+    fill.addColorStop(1, 'orange');
+
+    // Fill rectangle
+    context.fillStyle = fill;
+    context.fillRect(margin, margin, width - margin * 2, height - margin * 2);
+  };
+};
+
+// Start the sketch
+canvasSketch(sketch, settings);
